ea45ee27ada24770c8abcf34dbdfff341378825e
n00862205 <safarkaram@gmail.com>
2016-04-09 13:15:29 -0400

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -1,153 +1,165 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 //host name is 192.168.100.111
 class TCPClient
 {
-	public static void main(String argv[]) throws Exception
-	{
-		if(argv[0] != null)
-		{
-			//Declare variables
-			String hostname = argv[0];
-			String Option = "0";
-			int num = 2;
-			
-			//BufferedReader to get user input
-			BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-			
-			//Create Socket
-			Socket clientSocket = new Socket(hostname, 2541);
-			
-			//Send user input to the Server
-			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-			
-			//Get Server output 
-			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			
-			while(true)
-			{
-				System.out.println(Thread.activeCount());
-				
-				//Option Menu
-				Runnable rArray[] = new Runnable[75];
-				System.out.println("1. Host current Date and Time");
-				System.out.println("2. Host uptime");
-				System.out.println("3. Host memory use");
-				System.out.println("4. Host Netstat");
-				System.out.println("5. Host current users");
-				System.out.println("6. Host running processes");
-				System.out.println("7. Quit");
-				System.out.println("8. Light Load");
-				System.out.println("9. Heavy Load");
-				
-				//Get user Input
-				Option = inFromUser.readLine();
-				if(Option.equals("1") || Option.equals("2") || Option.equals("3") || Option.equals("4")
-						|| Option.equals("5") || Option.equals("6") || Option.equals("7") || Option.equals("8") || Option.equals("9"))
-				{
-					
-					if(Option.equals("8") || Option.equals("9"))
-					{
-						if(Option.equals("8")){
-							Option = "1";
-						}
-						else
-							Option = "4";
-						
-						Thread tArray[] = new Thread[num];
-						long timeArr = 0;
-						
-						for(int i = 0; i < num; i++)
-						{
-							rArray[i] = new MyThread(outToServer, inFromServer, Option + "\n", i);
-							tArray[i] = new Thread(rArray[i]);
-						}
-						
-						for(int i = 0; i < num; i++)
-						{
-							tArray[i].start();
-						}
-						
-						for(int i = 0; i < num; i++)
-						{
-							System.out.println(i);
-							tArray[i].join();
-						}
-					}
-					else if(!Option.equals("7"))
-					{
-						String Results = null;
-						outToServer.writeBytes(Option + '\n');
-						while((Results = inFromServer.readLine()) != null && Results.length() != 0 )
-						{
-							System.out.println(Results);
-						}
-					}
-					else
-					{
-						outToServer.writeBytes(Option + '\n');
-						break;
-					}
+   public static void main(String argv[]) throws Exception
+   {
+      if(argv.length > 0)
+      {
+      	//Declare variables
+         String hostname = argv[0];
+      	
+      	//BufferedReader to get user input
+         Scanner inFromUser = new Scanner(System.in);
+         Scanner inFromUser2 = new Scanner(System.in);
+         
+      	//Create Socket
+         Socket clientSocket = new Socket(hostname, 2541);
+      	
+      	//Send user input to the Server
+         PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+      	
+      	//Get Server output 
+         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      	
+         while(true)
+         {
+            String Option = "0";
+            int num = 1;
+            double avgTime = 0;
+         	
+         	//Option Menu
+            System.out.println("1. Host current Date and Time");
+            System.out.println("2. Host uptime");
+            System.out.println("3. Host memory use");
+            System.out.println("4. Host Netstat");
+            System.out.println("5. Host current users");
+            System.out.println("6. Host running processes");
+            System.out.println("7. Quit");
+            
+            //Get user Input
+            Option = inFromUser.nextLine();
+            
+            System.out.println("Enter Number of Threads");
+         	
+         	//Get number of threads
+            num = inFromUser2.nextInt();
+            
+            if(Option.equals("1") || Option.equals("2") || Option.equals("3") || Option.equals("4")
+            		|| Option.equals("5") || Option.equals("6"))
+            {
+            	threadTime time = new threadTime(num);
 
-				}
-				else
-					System.out.println("Invalid Input");
-			}
-			clientSocket.close();
-		}
-		else
-		{
-			System.out.println("No agruments given");
-		}
-	}	
+   				for(int i = 1; i < num + 1; i++) {
+   				
+   					// Create a new thread
+   					MyThread thread = new MyThread(outToServer, inFromServer, Option, time);
+   					thread.start();
+   					
+   					// Waits for all threads
+   					try	{
+   						thread.join();
+   					} catch(InterruptedException e) {
+   						System.out.println(e);
+   					}
+                  
+                  if(num == 1)
+                     System.out.println(thread.getResults());
+                  
+   					if ( (i % 5 == 0) || (i == 1) ) {
+   						avgTime = time.getAverage();
+   						System.out.println("Average time at " + i + " threads (in milliseconds): " + avgTime);
+   					}
+   				}
+            } else if(Option.equals("7")) {
+               outToServer.println(Option);
+               break;
+            }
+            else
+               System.out.println("Invalid Input");
+         }
+         
+         clientSocket.close();
+         inFromUser.close();
+         inFromUser2.close();
+      }
+      else
+      {
+         System.out.println("No agruments given");
+      }
+   }	
 }
 
-class MyThread implements Runnable{
-	private final DataOutputStream outToServer;
-	private final BufferedReader inFromServer;
-	private final String Option;
-	private final int a;
-	private long totalTime;
+class MyThread extends Thread {
+   private PrintWriter outToServer;
+   private BufferedReader inFromServer;
+   private String Option;
+   private threadTime time;
+   private String FullResults;
 
-	   public MyThread(DataOutputStream ots, BufferedReader ifs, String o, int x){
-	      outToServer = ots;
-	      inFromServer = ifs;
-	      Option =  o;
-	      a = x;
-	   }
+   public MyThread(PrintWriter ots, BufferedReader ifs, String o, threadTime t){
+      this.outToServer = ots;
+      this.inFromServer = ifs;
+      this.Option =  o;
+      this.time = t;
+   }
 
-	   public void run(){
-		   String Results;
-		   String FullResults = null;
-		   
-			long startTime = System.currentTimeMillis();
-			try {
-				outToServer.writeBytes(Option);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			try {
-				while((Results = inFromServer.readLine()) != null && Results.length() != 0 )
-				{
-					FullResults = FullResults + Results;
-					//System.out.println(Results);
-					Results = null;
-				}
-				long endTime = System.currentTimeMillis();
-				totalTime = endTime - startTime;
-				System.out.println("I am Thread " + a + " and my time " + totalTime);
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			return;
-	   }
-	   
-	   public long getTotalTime()
-	   {
-		   return totalTime;
-	   }
+   public void run(){
+	   long totalTime;
+	  	long startTime;
+		long endTime;
+		
+		startTime = System.currentTimeMillis();
+		
+		try {
+			outToServer.println(Option);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			FullResults = inFromServer.readLine();
+			endTime = System.currentTimeMillis();
+			totalTime = endTime - startTime;
+			time.add(totalTime);
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+
+	return;
 	}
+   
+   public String getResults(){
+      return FullResults;
+   }
+}
+
+class threadTime {
+	long[] times;
+   int counter;
+
+	public threadTime(int n) {
+      times = new long[n];
+		counter = 0;
+	}
+
+	public synchronized void add (long t) {
+		times[counter++] = t;
+	}
+	
+	
+	public double getAverage () {
+		double avg = 0;
+		
+		for (int i = 0; i < counter; i++)
+			avg += (double)times[i];
+	
+		avg = avg / (double)times.length;
+		
+		return avg;
+	}	
+}
